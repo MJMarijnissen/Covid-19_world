8db21c1b4431ec34462f5ed60d60ef3421ea37d1
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-17 10:04:40 +0100

    added functions check and top, extended functions to include yyear and added docstrings

--- covid_na_swiecie.py
+++ covid_na_swiecie.py
@@ -12,17 +12,60 @@ import numpy as np
 
 wczoraj = datetime.today().day-1
 miesiac = datetime.today().month
+rok = datetime.today().year-2000
 
 url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 df = pd.read_csv(url, error_bad_lines=False)
 
 
-def check(dzien, mies):
+def check(dzien, mies, rok):
+    """
+    Sprawdza ile wystąpień wirusa jest w Polsce
+
+    Parameters
+    ----------
+    dzien : INT
+        Day of the year
+    mies : INT
+        Month of the year
+    rok : INT
+        Last 2 digits of year e.g. for 2020, write 20
+
+    Returns
+    -------
+    None. Only prints results
+
+    """
 	df = pd.read_csv(url, error_bad_lines=False)
-	result = df.loc[df["Country/Region"]=="Poland"][f"{mies}/{dzien}/20"].values[0]
+	result = df.loc[df["Country/Region"]=="Poland"][f"{mies}/{dzien}/{rok}"].values[0]
 	print(result)
 	
- 
+
+def top(dzien, mies, rok):
+    """
+    Zwraca miejsca z największą liczbą przypadkóW
+
+    Parameters
+    ----------
+    dzien : INT
+        Day of the year
+    mies : INT
+        Month of the year
+    rok : INT
+        Last 2 digits of year e.g. for 2020, write 20
+
+    Returns
+    -------
+    None. Only prints results
+
+    """
+    data= f"{mies}/{dzien}/{rok}"
+    result = df[["Province/State", "Country/Region", data]].sort_values(by=data).tail(10)
+    print(result)
 
 print(f"data: {wczoraj}/{miesiac}")
-print("przypadki: ", check(wczoraj, miesiac))
+print(f"przypadki w Polsce: ")
+check(wczoraj, miesiac,rok)
+
+print("miejsca z największą liczbą przypadków: ")
+top(wczoraj,miesiac,rok)
