babe6a92774e792c91ecd63c5ef530b2ab8adf00
MJMarijnissen <kubamarijnissen@gmail.com>
2020-03-17 09:30:18 +0100

    added url

--- covid_na_swiecie.py
+++ covid_na_swiecie.py
@@ -5,3 +5,24 @@ Created on Tue Mar 17 09:25:42 2020
 @author: Kubus
 """
 
+import pandas as pd
+from datetime import datetime
+import numpy as np
+
+
+wczoraj = datetime.today().day-1
+miesiac = datetime.today().month
+
+url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+df = pd.read_csv(url, error_bad_lines=False)
+
+
+def check(dzien, mies):
+	df = pd.read_csv(url, error_bad_lines=False)
+	result = df.loc[df["Country/Region"]=="Poland"][f"{mies}/{dzien}/20"].values[0]
+	print(result)
+	
+ 
+
+print(f"data: {wczoraj}/{miesiac}")
+print("przypadki: ", check(wczoraj, miesiac))
